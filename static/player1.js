1f02422a27352fb90ec9901ee62ccec0da51267a
Dominik Wujek <dom.wujek@gmail.com>
2024-06-02 22:48:29 +0200

    play buttons got progress bar

--- static/player1.js
+++ static/player1.js
@@ -29,13 +29,15 @@ document.addEventListener('DOMContentLoaded', function() {
         if (playing == true){
             audio.pause();
             playing = false;
+            changeButtonText(currentPlayingButton, '▶')
             timeOnPause = audio.currentTime;
             playBtn.innerHTML = `<svg width="19" height="19" viewBox="-3 0 28 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>play</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-419.000000, -571.000000)" fill="#ffffff"> <path d="M440.415,583.554 L421.418,571.311 C420.291,570.704 419,570.767 419,572.946 L419,597.054 C419,599.046 420.385,599.36 421.418,598.689 L440.415,586.446 C441.197,585.647 441.197,584.353 440.415,583.554" id="play" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>`
             
         } else {
             if (currentPlayingButton){
                 playing = true;
-                currentPlayingButton.textContent = "⏸";
+                changeButtonText(currentPlayingButton, '⏸')
+                // currentPlayingButton.textContent = "⏸";
                 playBtn.innerHTML = `<svg width="17" height="17" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#c8c8c8"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 6C2 4.11438 2 3.17157 2.58579 2.58579C3.17157 2 4.11438 2 6 2C7.88562 2 8.82843 2 9.41421 2.58579C10 3.17157 10 4.11438 10 6V18C10 19.8856 10 20.8284 9.41421 21.4142C8.82843 22 7.88562 22 6 22C4.11438 22 3.17157 22 2.58579 21.4142C2 20.8284 2 19.8856 2 18V6Z" fill="#ffffff"></path> <path d="M14 6C14 4.11438 14 3.17157 14.5858 2.58579C15.1716 2 16.1144 2 18 2C19.8856 2 20.8284 2 21.4142 2.58579C22 3.17157 22 4.11438 22 6V18C22 19.8856 22 20.8284 21.4142 21.4142C20.8284 22 19.8856 22 18 22C16.1144 22 15.1716 22 14.5858 21.4142C14 20.8284 14 19.8856 14 18V6Z" fill="#ffffff"></path> </g></svg>`;
                 audio.play()
  
@@ -44,6 +46,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
+    function changeButtonText(button, newText) {
+        const spanElement = button.querySelector('.btn-txt');
+        console.log(button)
+        if (spanElement) {
+            spanElement.textContent = newText;
+        }
+    }
+
     var playButtons = document.querySelectorAll('.pick-btn');
     playButtons.forEach(function(button) {
         const audioUrl = button.getAttribute('data-audio-url')
@@ -51,15 +61,17 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', function() {
 
             if (currentPlayingButton && currentPlayingButton !== this) {
-                currentPlayingButton.textContent = "▶";
+                // currentPlayingButton.textContent = "▶";
+                changeButtonText(currentPlayingButton, '▶')
             }
             if (audio.src == new URL(audioUrl, window.location.href).href) {
                 if (playing == true) {
                 audio.pause();
+                changeButtonText(currentPlayingButton, '▶')
                 playing = false
                 playBtn.innerHTML = `<svg width="19" height="19" viewBox="-3 0 28 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>play</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-419.000000, -571.000000)" fill="#ffffff"> <path d="M440.415,583.554 L421.418,571.311 C420.291,570.704 419,570.767 419,572.946 L419,597.054 C419,599.046 420.385,599.36 421.418,598.689 L440.415,586.446 C441.197,585.647 441.197,584.353 440.415,583.554" id="play" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>`
             } else if (playing == false) {
-                this.textContent = "⏸";
+                changeButtonText(currentPlayingButton, '⏸')
                 playing = true;
                 playBtn.innerHTML = `<svg width="17" height="17" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#c8c8c8"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 6C2 4.11438 2 3.17157 2.58579 2.58579C3.17157 2 4.11438 2 6 2C7.88562 2 8.82843 2 9.41421 2.58579C10 3.17157 10 4.11438 10 6V18C10 19.8856 10 20.8284 9.41421 21.4142C8.82843 22 7.88562 22 6 22C4.11438 22 3.17157 22 2.58579 21.4142C2 20.8284 2 19.8856 2 18V6Z" fill="#ffffff"></path> <path d="M14 6C14 4.11438 14 3.17157 14.5858 2.58579C15.1716 2 16.1144 2 18 2C19.8856 2 20.8284 2 21.4142 2.58579C22 3.17157 22 4.11438 22 6V18C22 19.8856 22 20.8284 21.4142 21.4142C20.8284 22 19.8856 22 18 22C16.1144 22 15.1716 22 14.5858 21.4142C14 20.8284 14 19.8856 14 18V6Z" fill="#ffffff"></path> </g></svg>`;
                 audio.play()
@@ -71,8 +83,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 title.textContent = this.getAttribute("audio-name");
                 author.textContent = this.getAttribute('author');
                 cover.src = this.getAttribute('pic-url');
-                this.textContent = "⏸";
+                // this.textContent = "⏸";
+                changeButtonText(currentPlayingButton, '⏸')
             }
+            
         });
     });
 
@@ -81,7 +95,8 @@ document.addEventListener('DOMContentLoaded', function() {
         backBtn.addEventListener("click", ()=>{changeTrack(-1)})
 
         function changeTrack(x){
-            currentPlayingButton.textContent = "▶";
+            changeButtonText(currentPlayingButton, '▶')
+            currentPlayingButton.querySelector('div').style.width = `0%`
             nextIndex = playList.indexOf(currentPlayingButton.getAttribute('data-audio-url')) + x
 
             if (nextIndex > playList.length - 1){
@@ -92,7 +107,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
             const nextUrl = playList[nextIndex]
             currentPlayingButton = document.querySelector(`button[data-audio-url="${nextUrl}"]`);
-            currentPlayingButton.textContent = "⏸";
+            // currentPlayingButton.textContent = "⏸";
+            changeButtonText(currentPlayingButton, '⏸')
+            cover.src = currentPlayingButton.getAttribute('pic-url');
             
             play(nextUrl)
         }
@@ -100,9 +117,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     playBtn.addEventListener('click', playBtnClick)
 
-    audio.addEventListener("pause", ()=>{
-        currentPlayingButton.textContent = "▶"
-    })
 
     let lastUpdated;
     audio.addEventListener('timeupdate', updateProgress);
@@ -113,6 +127,11 @@ document.addEventListener('DOMContentLoaded', function() {
         const progress = (currentTime / duration) || 0; //this shit was making the prog bar jump in the middle oh my god
         progSlider.value = progress;
         updateSlider(progSlider);
+
+        const percentValue = progress * 100;
+        currentPlayingButton.querySelector('div').style.width = `${percentValue}%`
+
+
         const secNow = Math.floor(currentTime % 60);
         
         if (secNow !== lastUpdated){
